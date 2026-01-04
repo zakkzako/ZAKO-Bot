73c18b4729ef40a0b04c83ea3951cf5087d9a525
zakkzako <nyo48607@gmail.com>
2026-01-04 15:18:24 +0900

    Add new job types to JOB_MAP in work.py

--- work.py
+++ work.py
@@ -7,8 +7,16 @@ import os
 
 JST = pytz.timezone('Asia/Tokyo')
 JOB_MAP = {
-    "none": {"name": "無職", "time": 10}, "gambler": {"name": "ギャンブラー", "time": 10},
-    "engineer": {"name": "エンジニア", "time": 60} # 以前の表に基づいて適宜追加してください
+    "none": {"name": "無職", "time": 10},
+    "gambler": {"name": "ギャンブラー", "time": 10},
+    "chick_taxonomist": {"name": "ひよこ鑑定士", "time": 10},
+    "freeter": {"name": "フリーター", "time": 15},
+    "farmer": {"name": "農家", "time": 20},
+    "influencer": {"name": "インフルエンサー", "time": 5},
+    "architect": {"name": "建築家", "time": 45},
+    "investor": {"name": "投資家", "time": 30},
+    "engineer": {"name": "エンジニア", "time": 60},
+    "doctor": {"name": "医師", "time": 60}
 }
 
 async def handle_work_detection(bot, message, embed):
