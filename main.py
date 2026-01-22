4e8701031a9c5799c8ffda7db8c7bbc52b5a824e
Yamatomato0105 <yamatomato0105@gmail.com>
2026-01-22 23:09:11 +0900

    影響のない変更 EC申請の承認をボタンに変更するための準備

--- main.py
+++ main.py
@@ -121,6 +121,28 @@ class DiscordBotLogger(logging.Handler):
         except Exception as e:
             logger.error(f"Failed to send log via webhook: {e}")
 
+class EconomyApplication(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    """TC = TakasumiBOT Coin"""
+
+    # EC -> TC
+    @discord.ui.button(custom_id="ec-application:exchange:to_ec:approve", label="承認", style=discord.ButtonStyle.green)
+    async def ec_application_exchange_to_ec_approve(self, interaction: discord.Interaction, button: discord.ui.Button)
+        await core_system.handle_economy_application_exchange_ec(self, interaction, approved=True)
+    @discord.ui.button(custom_id="ec-application:exchange:to_ec:reject", label="拒否", style=discord.ButtonStyle.red)
+    async def ec_application_exchange_to_ec_reject(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await core_system.handle_economy_application_exchange_ec(self, interaction, approved=False)
+
+    # TC -> EC
+    @discord.ui.button(custom_id="ec-application:exchange:to_tc:approve", label="承認", style=discord.ButtonStyle.green)
+    async def ec_application_exchange_to_tc_approve(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await core_system.handle_economy_application_exchange_tc(self, interaction, approved=True)
+    @discord.ui.button(custom_id="ec-application:exchange:to_tc:reject", label="拒否", style=discord.ButtonStyle.red)
+    async def ec_application_exchange_to_tc_reject(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await core_system.handle_economy_application_exchange_tc(self, interaction, approved=False)
+
 bot = TakasumiAuxiliaryBot()
 
 @bot.event
