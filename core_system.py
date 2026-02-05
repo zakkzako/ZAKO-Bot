4e91c6750778fccd59915dec5ac8e78a1b1926b1
zakkzako <nyo48607@gmail.com>
2026-02-05 16:14:56 +0900

    失業保険失効通知のメッセージを変更

--- core_system.py
+++ core_system.py
@@ -73,7 +73,7 @@ async def check_reminders(bot):
         json.dump(updated_queue, f, indent=4)
 
     if item.get('notification_type') == 'unemployment':
-        await channel.send(f"<@{item['user_id']}> 失業保険の期限が切れました")
+        await channel.send(f"<@{item['user_id']}> 失業保険が失効しました")
 
 
 async def process_message_event(bot, message):
