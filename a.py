1f813944a0048ee1e0fad6d20fcd4194dd5f86c2
nyo-zakk <nyosabu40@gmail.com>
2026-03-21 09:57:13 +0900

    Delete a.py

--- a.py
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# .pyファイルのリストを表示し、最後に合計サイズを計算する
-echo "--- Python Files List ---"
-find . -name "*.py" -type f -exec du -ch {} + | grep -v "total$"
-
-echo "--------------------------"
-echo -n "Total Size: "
-find . -name "*.py" -type f -print0 | xargs -0 du -ch | tail -n 1 | cut -f 1
